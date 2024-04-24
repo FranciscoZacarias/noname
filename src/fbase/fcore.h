883ca81894329cd3c2b016fbc645a556d9fe4d0b
FranciscoZacarias <franciscobelchiorzacarias@gmail.com>
2024-04-24 23:12:26 +0200

    removed not needed

--- src/fbase/fcore.h
+++ src/fbase/fcore.h
@@ -2,7 +2,7 @@
 #ifndef FCORE_H
 #define FCORE_H
 
-// Start: Helper ----------------
+// Start: System ----------------
 
 #if ARCH_64BIT
 # define IntFromPtr(ptr) ((U64)(ptr))
@@ -22,36 +22,7 @@
 # error Unknown trap intrinsic for this compiler.
 #endif
 
-#if OS_WINDOWS
-# include <windows.h>
-# include <tmmintrin.h>
-# include <wmmintrin.h>
-# include <intrin.h>
-# if ARCH_X64
-#  define ins_atomic_u64_eval(x) InterlockedAdd64((volatile __int64 *)(x), 0)
-#  define ins_atomic_u64_inc_eval(x) InterlockedIncrement64((volatile __int64 *)(x))
-#  define ins_atomic_u64_dec_eval(x) InterlockedDecrement64((volatile __int64 *)(x))
-#  define ins_atomic_u64_eval_assign(x,c) InterlockedExchange64((volatile __int64 *)(x),(c))
-#  define ins_atomic_u64_add_eval(x,c) InterlockedAdd64((volatile __int64 *)(x), c)
-#  define ins_atomic_u64_eval_cond_assign(x,k,c) InterlockedCompareExchange64((volatile __int64 *)(x),(k),(c))
-#  define ins_atomic_u32_eval(x,c) InterlockedAdd((volatile LONG *)(x), 0)
-#  define ins_atomic_u32_eval_assign(x,c) InterlockedExchange((volatile LONG *)(x),(c))
-#  define ins_atomic_u32_eval_cond_assign(x,k,c) InterlockedCompareExchange((volatile LONG *)(x),(k),(c))
-#  define ins_atomic_ptr_eval_assign(x,c) (void*)ins_atomic_u64_eval_assign((volatile __int64 *)(x), (__int64)(c))
-# else
-#  error Atomic intrinsics not defined for this operating system / architecture combination.
-# endif
-#elif OS_LINUX
-# if ARCH_X64
-#  define ins_atomic_u64_inc_eval(x) __sync_fetch_and_add((volatile U64 *)(x), 1)
-# else
-#  error Atomic intrinsics not defined for this operating system / architecture combination.
-# endif
-#else
-# error Atomic intrinsics not defined for this operating system.
-#endif
-
-// End: Helper ------------------
+// End: System ------------------
 
 // Start: Core -------------------
 
