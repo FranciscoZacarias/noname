38ca93f98f9677ff352d5bc18f2aa7611d82b2c9
FranciscoZacarias <franciscobelchiorzacarias@gmail.com>
2024-04-24 22:51:50 +0200

    rename in buildbat

--- build.bat
+++ build.bat
@@ -6,7 +6,7 @@ set main_file=..\src\main.c
 REM Linker errors for GLFW. Use /MD on release and /MDd on debug builds
 set cl_default_flags=/Isrc /nologo /FC /Zi /MDd
 
-set external_include=/I"..\src\external\opengl" /I"..\src/external/glfw-3.3.9/include" /I"../src/flib"
+set external_include=/I"..\src\external\opengl" /I"..\src/external/glfw-3.3.9/include" /I"../src/fbase"
 set linker_flags=user32.lib gdi32.lib Shell32.lib opengl32.lib "..\src/external/glfw-3.3.9/lib/glfw3.lib"
 
 set executable=/Fe"%project_name%.exe"
