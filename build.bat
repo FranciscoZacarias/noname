024729af3610049b91e579200053254eb4afb425
Francisco zacarias <francisco.zacarias@thetoadman.com>
2024-04-26 10:53:01 +0200

    simpler build.bat

--- build.bat
+++ build.bat
@@ -1,21 +1,21 @@
 @echo off
 
-set project_name=noname
-set compiler=cl
-set main_file=..\src\main.c
+set compiler_and_entry=cl ..\src\main.c
+
 REM Linker errors for GLFW. Use /MD on release and /MDd on debug builds
 set cl_default_flags=/Isrc /nologo /FC /Zi /MDd
 
-set external_include=/I"..\src\external\opengl" /I"..\src/external/glfw-3.3.9/include" /I"../src/fbase"
-set linker_flags=user32.lib gdi32.lib Shell32.lib opengl32.lib "..\src/external/glfw-3.3.9/lib/glfw3.lib"
+set external_include= /I"..\src\external\opengl"             ^
+											/I"..\src\external\glfw-3.3.9\include" ^
+											/I"..\src\fbase"
 
-set executable=/Fe"%project_name%.exe"
+set linker_flags= user32.lib   ^
+									gdi32.lib    ^
+									Shell32.lib  ^
+									opengl32.lib ^
+									"..\src\external\glfw-3.3.9\lib\glfw3.lib"
 
 if not exist build mkdir build
 pushd build
-if "%1" == "-d" (
-	echo Deleting build/*
-	del /q *.*
-)
-%compiler% %main_file% %cl_default_flags% %external_include% %static_include% %linker_flags% %executable%
+%compiler_and_entry% %cl_default_flags% %external_include% %static_include% %linker_flags% /Fe"noname.exe"
 popd
