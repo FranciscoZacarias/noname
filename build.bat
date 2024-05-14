8d56652547eba16171dfaf9b2d1120e8e4e39711
Francisco zacarias <francisco.zacarias@thetoadman.com>
2024-05-14 14:12:18 +0200

    buid ltime was broken

--- build.bat
+++ build.bat
@@ -17,11 +17,5 @@ set linker_flags= user32.lib ^
 									"..\src\external\glfw-3.3.9\lib\glfw3.lib"
 
 if not exist build mkdir build
-pushd build
-	set start=%time%
-		%compiler_and_entry% %cl_default_flags% %external_include% %linker_flags% /Fe"noname.exe"
-	set end=%time%
-	set /a elapsed_seconds=(%end:~0,2%-%start:~0,2%)*60 + (%end:~3,2%-%start:~3,2%)
-	set /a milliseconds=(%end:~9,2%-%start:~9,2%)
-	echo Compile time: %elapsed_seconds%.%milliseconds%s
+	%compiler_and_entry% %cl_default_flags% %external_include% %linker_flags% /Fe"noname.exe"
 popd
